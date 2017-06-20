2ad3235a22e2a289c35b4bc3505be51f1a4c0291
clarkmcadoo <clark.mcadoo@gmail.com>
2017-06-20 09:06:33 -0500

    get 3 paragraphs

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const app = express();
 var loremIpsum = require("lorem-ipsum");
-var output = loremIpsum({count:3, 
-                        units: "paragraphs",
-                        at: "html"});
+output = loremIpsum({
+    count: 3                      // Number of words, sentences, or paragraphs to generate. 
+  , units: 'paragraphs'            // Generate words, sentences, or paragraphs. 
+  , sentenceLowerBound: 5         // Minimum words per sentence. 
+  , sentenceUpperBound: 15        // Maximum words per sentence. 
+  , paragraphLowerBound: 3        // Minimum sentences per paragraph. 
+  , paragraphUpperBound: 7        // Maximum sentences per paragraph. 
+  , format: 'html'               // Plain text or html 
+  , random: Math.random           // A PRNG function. Uses Math.random by default 
+
+});
 var port = 3000;
 
 app.get("/lorem", function(req, res){
